0ba4d9079eb5bb30ef6c4d18c3597fb693bf2507
Atharva Meher <92387222+atharvam2004@users.noreply.github.com>
2023-10-10 23:56:18 +0530

    Added solution - LeetHub

---
+++ Shop in Candy Store - GFG/shop-in-candy-store.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(input_line[0]);
+            int K = Integer.parseInt(input_line[1]);
+            input_line = read.readLine().trim().split("\\s+");
+            int candies[]= new int[N];
+            for(int i = 0; i < N; i++)
+                candies[i] = Integer.parseInt(input_line[i]);
+
+            Solution ob = new Solution();
+            ArrayList<Integer> cost = ob.candyStore(candies,N,K);
+            System.out.println(cost.get(0)+" "+cost.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    static ArrayList<Integer> candyStore(int candies[],int N,int K){
+        // code here
+        Arrays.sort(candies);
+        int i=0,j=candies.length-1;
+        int max=0,min=0;
+        while(i<=j){
+            min+=candies[i++];
+            j-=K;
+        }
+        i=0;j=candies.length-1;
+         while(i<=j){
+            max+=candies[j--];
+            i+=K;
+        }
+        ArrayList <Integer>ans=new ArrayList<>();
+        ans.add(min);
+        ans.add(max);
+        return ans;
+    }
+}
