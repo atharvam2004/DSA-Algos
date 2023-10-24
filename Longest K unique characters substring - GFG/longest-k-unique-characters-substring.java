0a5cc16ade24be9ae21964f8b6b4aaed8aaa6119
Atharva Meher <92387222+atharvam2004@users.noreply.github.com>
2023-10-24 17:49:41 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.java
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.java
@@ -22,28 +22,32 @@ class GfG {
 // User function Template for Java
 
 class Solution {
-    public int longestkSubstr(String s, int k) {
-        HashMap <Character,Integer>map=new HashMap<>();
-        int i=0,j=0;
-        int ans=-1;
-        while(i<s.length()){
-            if(j<s.length()&&map.size()<=k){
-                char ch=s.charAt(j);
-                map.put(ch,map.getOrDefault(ch,0)+1);
-                j++;
-                if(map.size()==k){
-                    ans=Math.max(ans,j-i);
-                }
-            }
-            else if(i<s.length()&&map.size()>k){
-                char ch=s.charAt(i);
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==0)map.remove(ch);
-                i++;
-            }else{
-                return ans;
-            }
-        }
-        return ans;
+   public int longestkSubstr(String str, int k) {
+        // code here
+        int n=str.length();
+		HashMap<Character,Integer>map=new HashMap<>();
+		
+		int i=0,j=0,max=-1;
+		while(j<n) {
+			Integer J=map.get(str.charAt(j));
+			map.put(str.charAt(j),(J==null)?1:J+1 );
+			if(map.size()==k) {
+				max=Math.max(max,j-i+1);						
+			}
+			if(map.size()>k) {
+				max=Math.max(max, j-i);
+				while(map.size()>k) {
+					J=map.get(str.charAt(i));
+					if(J-1==0)
+						map.remove(str.charAt(i));
+					else
+						map.put(str.charAt(i), J-1);
+					i++;
+				}
+			}
+			j++;
+
+		}
+        return max;
     }
 }
