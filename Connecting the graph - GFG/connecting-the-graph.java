a2ad145f0e8eaf393058b0f3cb24034058dbdf2f
Atharva Meher <meheratharva49@gmail.com>
2023-11-14 15:49:43 +0530

    Added solution - LeetHub

---
+++ Connecting the graph - GFG/connecting-the-graph.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] edge = new int[m][2];
+            for (int i = 0; i < m; i++) {
+                edge[i][0] = sc.nextInt();
+                edge[i][1] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            int ans = obj.Solve(n, edge);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    int cycle=0;
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v){
+            cycle++;
+            return;
+        }
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+}
+class Solution {
+
+    public int Solve(int n, int[][] edge) {
+        int size=n;
+        DisjointSet d=new DisjointSet(size);
+        for(int i=0;i<edge.length;i++){
+            d.unionByRank(edge[i][0],edge[i][1]);
+        }
+        int ans=-1;
+        for(int i=0;i<size;i++){
+           if(d.parent.get(i)==i){
+                ans++;
+                d.cycle--;
+                if(d.cycle<-1)return -1;
+           }
+        }
+        return ans;
+    }
+}
